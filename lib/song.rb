a167b4ceb3057ab0ab49e8cd98e266d472a6775d
John Casabar <johncasabar@gmail.com>
2018-10-19 04:15:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
 class Song
-  extend Memorable, Findable
+  extend Memorable::ClassMethods, Findable
+  include Memorable::InstanceMethods
 
   attr_accessor :name
   attr_reader :artist
